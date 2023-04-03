6587cd34289f166c7c21b6ef3da9d2359f385913
Rinu Sakeer <renarinu7823@gmail.com>
2023-04-03 16:21:52 +0530

    added service section

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var menuBtn = document.getElementById("menu-btn")
-var sideNav = document.querySelectorAll('#sideNav')
+var sideNav = document.getElementById("sideNav")
 var menu = document.getElementById("menu")
 
 sideNav.style.right = "-250px";
@@ -7,10 +7,12 @@ sideNav.style.right = "-250px";
 menuBtn.onclick = function(){
     if(sideNav.style.right == "-250px"){
         sideNav.style.right = "0";
+        menu.src = "close.png";
 
     }
     else{
         sideNav.style.right = "-250px";
+        menu.src = "menubar.png";
 
     }
 }
